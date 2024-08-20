b53c86cd235d1d9839fc10ceee4aca46f009d942
Clarence <clarencekaiwen@gmail.com>
2024-08-21 02:12:15 +0800

    Update tab completion

--- src/main/java/com/clarence/punish/PunishTabCompleter.java
+++ src/main/java/com/clarence/punish/PunishTabCompleter.java
@@ -5,7 +5,6 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
-import org.bukkit.entity.Player;
 import org.bukkit.util.StringUtil;
 
 import java.util.ArrayList;
@@ -14,31 +13,36 @@ import java.util.List;
 public class PunishTabCompleter implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-       if (args.length == 1) {
-           List<String> options = new ArrayList<>();
-           options.add("version");
-           options.add("lookup");
+        if (args.length == 1) {
+            List<String> options = new ArrayList<>();
+            options.add("version");
+            options.add("lookup");
+            return stringUtil(args, options, 0);
+        }
 
-           for (Player player : Bukkit.getOnlinePlayers()) {
-               options.add(player.getDisplayName());
-           }
-           return StringUtil.copyPartialMatches(args[0], options, new ArrayList<>());
-       } else if (args.length == 2) {
-           List<String> commonReason = new ArrayList<>();
-           for (OfflinePlayer uuid : Bukkit.getServer().getOfflinePlayers()) {
-               commonReason.add(uuid.getUniqueId().toString());
-           }
-           commonReason.add("Hacking");
-           commonReason.add("Xraying");
-           commonReason.add("Griefing");
-           commonReason.add("Spamming");
-           commonReason.add("Flying");
-           commonReason.add("Bullying others");
-           commonReason.add("Harassment");
+        if (args[0].equals("lookup")) {
+            List<String> players = new ArrayList<>();
 
-
-           return StringUtil.copyPartialMatches(args[1], commonReason, new ArrayList<>());
-       }
+            for (OfflinePlayer offlinePlayer : Bukkit.getServer().getOfflinePlayers()) {
+                players.add(offlinePlayer.getUniqueId().toString());
+            }
+            return stringUtil(args, players, 1);
+        }
+      // if (args.length == 2) {
+      //    List<String> commonReason = new ArrayList<>();
+      //    commonReason.add("Hacking");
+      //    commonReason.add("Xraying");
+      //    commonReason.add("Griefing");
+      //    commonReason.add("Spamming");
+      //    commonReason.add("Flying");
+      //    commonReason.add("Bullying others");
+      //    commonReason.add("Harassment");
+      //    return StringUtil.copyPartialMatches(args[1], commonReason, new ArrayList<>());
+      //}
         return null;
+    }
+
+    private ArrayList<String> stringUtil (String[] args, List<String> options, int argIndex) {
+        return StringUtil.copyPartialMatches(args[argIndex], options, new ArrayList<>());
     }
 }
